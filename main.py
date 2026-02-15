ea857c71c16dcd3ce38ded6a7b59d05653367732
unknown <muhammadyarkhan31@gmail.com>
2026-02-15 10:38:34 -0800

    Second Push

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #pycaret library is used to implement explainable ai with fewer lines of code
 
 from pycaret.datasets import get_data
+from pycaret.classification import *
 data = get_data('diabetes')
+
+s = setup(data,target='Class variable',session_id=43)
+
+
