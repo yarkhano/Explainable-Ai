17fac549697d6086fe6ec7b28cc0f8e28827b679
unknown <muhammadyarkhan31@gmail.com>
2026-02-16 03:01:21 -0800

    Adding main code

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ data = get_data('diabetes')
 s = setup(data,target='Class variable',session_id=43)
 
 
+best_model = compare_models()
+print(best_model)
+
+print("Creating model  Random Forest,as lrp is good but random forest is complex so it is selected")
+model = create_model('rf')
+
+print('Generating summary plot (which show which column is important for the whole dataset)')
+interpret_model(model,plot='summary')
+
+print('Generating reason plot,it tells the reason for a specific patient')
+interpret_model(model,plot='reason')
